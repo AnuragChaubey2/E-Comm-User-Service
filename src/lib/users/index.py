93cd7f470f8babe3c516ac9c48a920479f1170b1
Anurag <anuragchaubey2@gmail.com>
2023-12-22 00:44:10 +0530

    Commits:

--- src/lib/users/index.py
+++ src/lib/users/index.py
@@ -1,17 +1,17 @@
 from pymongo import MongoClient
-from bson import ObjectId
 from fastapi import HTTPException
+import os
+from dotenv import load_dotenv
 
-MONGO_DB_URL = "mongodb://localhost:27017"
-MONGO_DB_NAME = "e-commerce"
-MONGO_COLLECTION_NAME = "users"
+load_dotenv()
 
-mongo_client = MongoClient(MONGO_DB_URL)
-db = mongo_client[MONGO_DB_NAME]
-collection = db[MONGO_COLLECTION_NAME]
+mongo_client = MongoClient(os.getenv('MONGO_DB_URL'))
+db = mongo_client[os.getenv('MONGO_DB_NAME')]
+collection = db[os.getenv('MONGO_COLLECTION_NAME')]
 
-async def get_user_by_id(user_id: str):
-    user = collection.find_one({"_id": ObjectId(user_id)})
+async def get_user_by_id(query):
+    user = collection.find_one(query, {'_id': 0})
     if user:
         return user
-    raise HTTPException(status_code=404, detail="User not found")
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel,Field
 from fastapi import APIRouter, HTTPException
 from src.lib.users.index import get_user_by_id
-import re
 
 router = APIRouter(redirect_slashes=False)
 
@@ -13,44 +12,20 @@ class Address(BaseModel):
     street: str
     city: str
     state: str
-    pincode: str
-    building_no: str
+    zip_code: str
+    building_no: str = Field(default="")
 
 class Users(BaseModel):
-    name: str
+    user_id: str
     email: str
     full_name: str
     phone_number: str
     date_of_birth: str
     address: Address
 
-    @validator('email', always=True)
-    def check_email_required(cls, value):
-        if not value:
-            raise ValueError('Email Field is required')
-        return value
-
-    @validator('email', always=True)
-    def check_valid_email(cls, value):
-        if not re.match(EMAIL_REGEX, value):
-            raise ValueError('Email is Invalid')
-        return value
-
-    @validator('phone_number', always=True)
-    def check_phone_required(cls, value):
-        if not value:
-            raise ValueError('Phone Field is required')
-        return value
-
-    @validator('phone_number', always=True)
-    def check_valid_phone(cls, value):
-        if not re.match(PHONE_REGEX, value):
-            raise ValueError('Invalid Phone Number')
-        return value
-
 @router.get("/users/{user_id}", response_model=Users)
 async def get_user_by_id_endpoint(user_id: str):
-    user = get_user_by_id(user_id)
+    user = await get_user_by_id({"user_id": user_id})
     if user:
         return user
     raise HTTPException(status_code=404, detail="User not found")
