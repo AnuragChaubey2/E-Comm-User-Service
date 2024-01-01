c356cd823ff2be33f2c16f6a08800f4a11a15687
Anurag <anuragchaubey2@gmail.com>
2024-01-01 23:43:22 +0530

    New Dev

--- service/user.py
+++ service/user.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException
 from src.lib.users.index import get_user_by_email, get_user_by_phone
 
+
 async def check_duplicate_email_and_phone(updated_user_data, user_id):
     existing_email_user = await get_user_by_email(updated_user_data.email)
     if existing_email_user and existing_email_user["user_id"] != user_id:
