440354216436f418c25d37aa2093662c073390d3
Anurag <anuragchaubey2@gmail.com>
2024-01-06 12:51:49 +0530

    Added Middleware

--- src/lib/users/index.py
+++ src/lib/users/index.py
@@ -10,10 +10,12 @@ db = mongo_client[os.getenv('MONGO_DB_NAME')]
 collection = db[os.getenv('MONGO_COLLECTION_NAME')]
 
 
+# ---------------------------Create---------------------------
 async def save_user_to_mongo(user_data):
     result = collection.insert_one(user_data)
     return result.inserted_id
 
+# ---------------------------GetByID---------------------------
 async def get_user_by_id(query):
     user = collection.find_one(query, {'_id': 0})
     if user:
@@ -21,14 +23,21 @@ async def get_user_by_id(query):
     else:
         raise HTTPException(status_code=404, detail="User not found")
     
+# ---------------------------GetAll---------------------------    
 async def getAllUsers():
     users_cursor = collection.find({})
     users_list = list(users_cursor)
     return users_list
 
+# ---------------------------Delete---------------------------
 async def removeUser(query):
     result = collection.delete_one(query)
     return result.deleted_count == 1
+
+# ---------------------------Update---------------------------
+async def UpdatedUser(updated_user_data: dict) -> bool:
+    result = collection.update_one({"user_id": updated_user_data["user_id"]}, {"$set": updated_user_data})
+    return result.modified_count > 0
 
 async def get_user_by_email(email: str):
     user = collection.find_one({"email": email}, {'_id': 0})
@@ -37,7 +46,3 @@ async def get_user_by_email(email: str):
 async def get_user_by_phone(phone: str):
     user = collection.find_one({"phone_number": phone}, {'_id': 0})
     return user
-
-async def UpdatedUser(updated_user_data: dict) -> bool:
-    result = collection.update_one({"user_id": updated_user_data["user_id"]}, {"$set": updated_user_data})
-    return result.modified_count > 0
